c165d5914dc3f596f17e93fbbf7f916b8c167ef6
Max <122988460+MaximusMixik@users.noreply.github.com>
2024-02-03 11:46:48 +0200

    Add files via upload

--- simple/js/app.js
+++ simple/js/app.js
@@ -4515,7 +4515,7 @@
         da.init();
         const root = document.documentElement;
         const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-        const marqueeContent = document.querySelector("ul.marquee-content");
+        const marqueeContent = document.querySelector(".sponsors__marquee");
         root.style.setProperty("--marquee-elements", marqueeContent.children.length);
         for (let i = 0; i < marqueeElementsDisplayed; i++) marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
         window["FLS"] = false;

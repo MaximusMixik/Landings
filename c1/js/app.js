28b388d8430003efbd304aecd264f60e4be2babc
Max <122988460+MaximusMixik@users.noreply.github.com>
2024-07-31 16:25:41 +0300

    Add files via upload

---
+++ c1/js/app.js
@@ -0,0 +1,13 @@
+(() => {
+    "use strict";
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    window["FLS"] = true;
+})();
